db7ec152af315fdb4ff70b2f46c1a9c781fb4f36
wrjones104 <wrjones@gmail.com>
2025-10-31 15:07:22 -0400

    Added new icons

--- android/app/src/main/java/com/jones/aptracker/ui/IconPicker.kt
+++ android/app/src/main/java/com/jones/aptracker/ui/IconPicker.kt
@@ -4,13 +4,23 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Bolt
 import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material.icons.outlined.Code
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Flag
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material.icons.outlined.SportsEsports
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.outlined.Token
 import androidx.compose.material.icons.outlined.VideogameAsset
+import androidx.compose.material.icons.outlined.Report
+import androidx.compose.material.icons.outlined.FlutterDash
+import androidx.compose.material.icons.outlined.Anchor
+import androidx.compose.material.icons.outlined.Thunderstorm
+import androidx.compose.material.icons.outlined.PriorityHigh
+import androidx.compose.material.icons.outlined.Elderly
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material.icons.outlined.Landscape
 import androidx.compose.ui.graphics.vector.ImageVector
 
 object AppIcons {
@@ -18,11 +28,21 @@ object AppIcons {
         "default_icon" to Icons.Outlined.Home,
         "star" to Icons.Outlined.Star,
         "videogame" to Icons.Outlined.VideogameAsset,
+        "token" to Icons.Outlined.Token,
+        "heart" to Icons.Outlined.Favorite,
+        "anchor" to Icons.Outlined.Anchor,
         "sports" to Icons.Outlined.SportsEsports,
         "person" to Icons.Outlined.Person,
+        "elder" to Icons.Outlined.Elderly,
+        "thumb_up" to Icons.Outlined.ThumbUp,
+        "landscape" to Icons.Outlined.Landscape,
         "place" to Icons.Outlined.Place,
+        "priority" to Icons.Outlined.PriorityHigh,
+        "thunderstorm" to Icons.Outlined.Thunderstorm,
+        "report" to Icons.Outlined.Report,
         "flag" to Icons.Outlined.Flag,
         "build" to Icons.Outlined.Build,
+        "flutterdash" to Icons.Outlined.FlutterDash,
         "code" to Icons.Outlined.Code,
         "bolt" to Icons.Outlined.Bolt
     )
