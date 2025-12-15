199432209d90cf1bb4d8ff01053ab9bab4e1d9d9
wrjones104 <wrjones@gmail.com>
2025-12-15 12:09:37 -0500

    fix import on historyscreen

--- android/app/src/main/java/com/jones/aptracker/ui/HistoryScreen.kt
+++ android/app/src/main/java/com/jones/aptracker/ui/HistoryScreen.kt
@@ -87,6 +87,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
