e16902677bec4e4f9c5853068b5ef0059ded6746
wrjones104 <wrjones@gmail.com>
2026-06-19 06:20:35 -0400

    increment app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.jones.aptracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 55
-        versionName = "1.6.12"
+        versionCode = 56
+        versionName = "1.6.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.jones.aptracker"
         buildConfigField(
